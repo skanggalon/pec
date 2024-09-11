a6ef5f45c4ecc65e6d0b008f082f008be73e60cd
z41d123@gmail.com <z41d123@gmail.com>
2024-09-11 10:17:22 +0700

    [SELESAI] Menghapus output npm

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ def show_main(request):
     context = {
         'name_aplication': 'Pacil Electronic Center Depok',
         'name': 'Muhammad Zaid Ats Tsabit',
-        'npm' : '2306224410',
+        # 'npm' : '2306224410',
         'class': 'PBP D'
     }
     return render(request, "main.html", context)
